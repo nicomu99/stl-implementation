7b1e13fb47524d390694bc219df8734d0a1a9916
RavanEternal <RavanEternal@gmail.com>
2024-05-03 12:07:28 +0200

    initial commit

--- lu_crtp.py
+++ lu_crtp.py
@@ -1,2 +1,15 @@
+#input:
+#A is a numpy matrix mxn
+#P_c is a numpy permutation matrix nxn?
+#k is an integer
+#output:
+#Q_k is a numpy matrix
+#R_k is a numpy matrix
+def lu_crtp_4(A, P_c, k):
+    AP_c = np.dot(A, P_c)
+    AP_c_selected_columns = AP_c[:, :k]
+    Q_k, R_k = np.linalg.qr(AP_c_selected_columns)
+    return Q_k, R_k
+
 def lu_crtp():
     pass

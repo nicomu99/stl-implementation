2d0f5682d10d431baa569c239a225e1f2a3cab8a
mahdyfalah <mahdyfalah@gmail.com>
2024-05-03 10:57:30 +0200

    added numpy and main function

--- lu_crtp.py
+++ lu_crtp.py
@@ -1,2 +1,18 @@
-def lu_crtp():
-    pass
+import numpy as np
+
+def lu_crtp(matrix, rank):
+    for row in matrix:
+        print(row)
+
+def main():
+    matrix = np.array([
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ])
+
+    rank = 1
+    
+    lu_crtp(matrix, rank)
+
+main()
